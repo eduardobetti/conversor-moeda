779daa620e3701872c26efd28b1e81d3ef9d3964
eduardobetti <esbetti92@gmail.com>
2024-03-08 00:54:59 -0300

    pequenas alterações em style.css // script.js segue um desafio

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const chk = document.getElementById('chk')
+const valueIn = document.getElementById('valueIn')
 
 chk.addEventListener('change', () => {
   document.body.classList.toggle('dark')
+})
+
+valueIn.addEventListener('change', function(){
+  console.log('troquei o valor')
 })
